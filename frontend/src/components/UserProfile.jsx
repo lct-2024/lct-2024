d6390990bc3c6a7111d2da36d3a553eff4dd2b2b
Aspir01 <nikitovicaleks488@gmail.com>
2024-05-31 08:53:15 +0300

    Update UserProfile.jsx

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,22 +1,12 @@
 import React, { useState, useEffect } from "react"
 import axios from "axios"
 
-const config = {
-    "servers": [
-        {
-            "name": "default",
-            "url": "https://passport.lct24.dev.40ants.com/"
-        }
-    ]
-}
-
 const UserProfile = () => {
     const [userProfile, setUserProfile] = useState(null)
 
     useEffect(() => {
         const fetchUserProfile = async () => {
             try {
-                const serverUrl = config.servers[0].url
                 const token = document.cookie.replace(
                     /(?:(?:^|.*;s*)tokens*=s*([^;]*).*$)|^.*$/,
                     "$1"
@@ -30,8 +20,8 @@ const UserProfile = () => {
                     "Authorization": `Bearer ${token}`
                 }
 
-                const response = await axios.post(
-                    `${serverUrl}api/my_profile`,
+                const response = await axios.get(
+                    `https://passport.lct24.dev.40ants.com/api/my_profile`,
                     {
                         jsonrpc: "2.0",
                         method: "my_profile",
@@ -42,7 +32,7 @@ const UserProfile = () => {
                         headers: headers
                     }
                 )
-
+                console.log(response)
                 setUserProfile(response.data.result)
             } catch (error) {
                 console.error("Error:", error)
